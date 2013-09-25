e97d1169c0829bd10ae378ad7c400444fa734d68
Philip Harrison <philip@mailharrison.com>
2013-09-25 15:41:54 +0100

    Update template module

--- ng-gc-date-range-filters-directive.js
+++ ng-gc-date-range-filters-directive.js
@@ -7,27 +7,26 @@
 (function(){
 'use strict';
 
+angular.module('ng-gc-date-range-filters-template.html', []).run(function($templateCache) {
+  $templateCache.put('ng-gc-date-range-filters-template.html',
+    '<div class="u-inline"><label class="label filter-bar__label u-margin-Hxxs" for="filter-after">Created between</label><pikaday id="filter-after" name="after" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="Start date" aria-label="Created between: start date" allow-paste-date="true" date="filters.after" filter-data="{ after: (date | toDateTime) }"></pikaday><label class="label filter-bar__label u-margin-Hxxs" for="filter-before">&amp;</label><pikaday id="filter-before" name="before" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="End date" aria-label="Created between: end date" date="filters.before" filter-data="{ before: (date | toDateTime:true) }"></pikaday></div>');
+});
+
 'use strict';
 
 angular.module('gc.dateRangeFiltersDirective', [
   'gc.pikaday',
   'ngGcToDateTime',
   'gc.filterDataDirective',
-  'app/shared/components/date-range-filters/date-range-filters-template.html'
+  'ng-gc-date-range-filters-template.html'
 ]).directive('dateRangeFilters', function dateRangeFiltersDirective() {
   return {
     restrict: 'E',
     replace: true,
-    templateUrl: 'app/shared/components/date-range-filters/' +
-                 'date-range-filters-template.html',
+    templateUrl: 'ng-gc-date-range-filters-template.html',
     scope: {
       filters: '='
     }
   };
-});
-
-angular.module('date-range-filters-template.html', []).run(function($templateCache) {
-  $templateCache.put('date-range-filters-template.html',
-    '<div class="u-inline"><label class="label filter-bar__label u-margin-Hxxs" for="filter-after">Created between</label><pikaday id="filter-after" name="after" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="Start date" aria-label="Created between: start date" allow-paste-date="true" date="filters.after" filter-data="{ after: (date | toDateTime) }"></pikaday><label class="label filter-bar__label u-margin-Hxxs" for="filter-before">&amp;</label><pikaday id="filter-before" name="before" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="End date" aria-label="Created between: end date" date="filters.before" filter-data="{ before: (date | toDateTime:true) }"></pikaday></div>');
 });
 })();
