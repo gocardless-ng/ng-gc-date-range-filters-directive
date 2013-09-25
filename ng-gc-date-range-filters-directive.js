fa8143fe0cd4f86b040be02f2d4d19f51d75d522
Philip Harrison <philip@mailharrison.com>
2013-09-25 15:24:35 +0100

    Initial

---
+++ ng-gc-date-range-filters-directive.js
@@ -0,0 +1,33 @@
+/**
+ * @license ng-gc-date-range-filters-directive v0.1.0
+ * (c) 2013-2013 GoCardless, Ltd.
+ * https://github.com/gocardless-ng/ng-gc-date-range-filters-directive.git
+ * License: MIT
+ */
+(function(){
+'use strict';
+
+'use strict';
+
+angular.module('gc.dateRangeFiltersDirective', [
+  'gc.pikaday',
+  'ngGcToDateTime',
+  'gc.filterDataDirective',
+  'app/shared/components/date-range-filters/date-range-filters-template.html'
+]).directive('dateRangeFilters', function dateRangeFiltersDirective() {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'app/shared/components/date-range-filters/' +
+                 'date-range-filters-template.html',
+    scope: {
+      filters: '='
+    }
+  };
+});
+
+angular.module('date-range-filters-template.html', []).run(function($templateCache) {
+  $templateCache.put('date-range-filters-template.html',
+    '<div class="u-inline"><label class="label filter-bar__label u-margin-Hxxs" for="filter-after">Created between</label><pikaday id="filter-after" name="after" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="Start date" aria-label="Created between: start date" allow-paste-date="true" date="filters.after" filter-data="{ after: (date | toDateTime) }"></pikaday><label class="label filter-bar__label u-margin-Hxxs" for="filter-before">&amp;</label><pikaday id="filter-before" name="before" ng-class="\'input input--small filter-bar__pikaday\'" placeholder="End date" aria-label="Created between: end date" date="filters.before" filter-data="{ before: (date | toDateTime:true) }"></pikaday></div>');
+});
+})();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# ng-gc-date-range-filters-directive
+
+![](https://circleci.com/gh/gocardless-ng/ng-gc-date-range-filters-directive.png?circle-token=:circle-token)
+
+## Copyright and license
+
+Copyright 2013 GoCardless, Inc under the MIT license.
